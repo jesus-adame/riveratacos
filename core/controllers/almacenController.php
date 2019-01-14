2ab525246154b95ef2f7917e459650fe84f6ea0c
JESUS <jesus.adame.sand@gmail.com>
2019-01-13 20:50:41 -0600

    Nueva versión del proyecto

--- core/controllers/almacenController.php
+++
@@ -1,5 +0,0 @@
-<?php
-$html = new Smarty();
-
-$html->assign('titulo', 'Control de Inventarios');
-$html->display('views/templates/almacen.html');
--- core/models/Cuenta.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Cuenta {
-  public $id;
-  public $nombre;
-  public $mesero;
-  public $total;
-  public $estado;
-}
